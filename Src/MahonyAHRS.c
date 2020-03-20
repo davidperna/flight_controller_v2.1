9d3b1700484358694e11aad878f3476a3b914306
David <davidcperna@gmail.com>
2020-03-20 14:22:44 -0400

    Tuned Mahony filter
    Added the correct unit conversion to Mahony filter and tuned the P,I gains to a decent starting point

--- Src/MahonyAHRS.c
+++ Src/MahonyAHRS.c
@@ -28,8 +28,8 @@
 // Definitions
 
 #define DEFAULT_SAMPLE_FREQ	512.0f	// sample frequency in Hz
-#define twoKpDef	(2.0f * 0.5f)	// 2 * proportional gain
-#define twoKiDef	(2.0f * 0.0f)	// 2 * integral gain
+#define twoKpDef	(100.0f * 0.5f)	// 2 * proportional gain
+#define twoKiDef	(10.0f * 0.0f)	// 2 * integral gain
 
 
 //============================================================================================
